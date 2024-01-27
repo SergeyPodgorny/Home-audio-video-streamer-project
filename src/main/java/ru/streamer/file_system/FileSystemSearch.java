ddce108bfd59adabc07f600ce855488d2fbc4d4c
SergeyPodgorny <serkonpod@gmail.com>
2024-01-27 15:20:14 +0300

    Search file method implemented

--- src/main/java/ru/streamer/file_system/FileSystemSearch.java
+++ src/main/java/ru/streamer/file_system/FileSystemSearch.java
@@ -1,4 +1,10 @@
 package ru.streamer.file_system;
 
+import java.util.Set;
+
 public interface FileSystemSearch {
+
+    Set<String> searchFiles(String dir, int depth);
+
+
 }
--- src/main/java/ru/streamer/file_system/impl/StreamFileSystemSearch.java
+++ src/main/java/ru/streamer/file_system/impl/StreamFileSystemSearch.java
@@ -2,12 +2,26 @@ package ru.streamer.file_system.impl;
 
 import ru.streamer.file_system.FileSystemSearch;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamFileSystemSearch implements FileSystemSearch {
 
 
-
-
-
-
-
+    @Override
+    public Set<String> searchFiles(String dir, int depth) {
+        try(Stream<Path> stream = Files.walk(Paths.get(dir), depth)){
+            return stream.filter(file -> !Files.isDirectory(file))
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
