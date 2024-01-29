e5fb20c2b24a3a8a07dd9939a7716abc8d71be8d
SergeyPodgorny <serkonpod@gmail.com>
2024-01-29 20:00:13 +0300

    refactoring

--- src/main/java/ru/streamer/file_system/impl/PlayListPresenceCheck.java
+++ src/main/java/ru/streamer/file_system/impl/PlayListPresenceCheck.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.streamer.file_system.PlayListPresence;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 
-import static ru.streamer.constants.PathConstants.CURRENT_DIRECTORY;
 import static ru.streamer.constants.PathConstants.CURRENT_PATH;
 
 @Component
