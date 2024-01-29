673806fb35e6528553c65593ce3b924c2f3aa477
SergeyPodgorny <serkonpod@gmail.com>
2024-01-29 19:23:25 +0300

    isPlayListPresent() developed

--- src/main/java/ru/streamer/file_system/impl/PlayListPresenceCheck.java
+++ src/main/java/ru/streamer/file_system/impl/PlayListPresenceCheck.java
@@ -1,14 +1,26 @@
 package ru.streamer.file_system.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import ru.streamer.file_system.PlayListPresence;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+
+import static ru.streamer.constants.CurrentJarFolderRoute.CURRENT_DIRECTORY;
+
+@Component
+@Slf4j
 public class PlayListPresenceCheck implements PlayListPresence {
-
-
-
 
     @Override
     public boolean isPlayListPresent() {
-        return false;
+        if(Files.exists(Path.of(CURRENT_DIRECTORY))){
+            log.info("Play list found");
+            return true;
+        } else {
+            log.info("Play list not found");
+            return false;
+        }
     }
 }
