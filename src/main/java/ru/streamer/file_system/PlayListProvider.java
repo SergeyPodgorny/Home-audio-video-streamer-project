c2c80e2d717e9b7f7ac63a06537e2fd279b6ce2a
SergeyPodgorny <serkonpod@gmail.com>
2024-01-30 18:52:47 +0300

    PlayListProvider type and basic implementation provided

---
+++ src/main/java/ru/streamer/file_system/PlayListProvider.java
@@ -0,0 +1,11 @@
+package ru.streamer.file_system;
+
+import java.util.Map;
+
+public interface PlayListProvider {
+
+
+    Map<String,String> providePlayList();
+
+
+}
---
+++ src/main/java/ru/streamer/file_system/impl/FileSystemPlayListProvider.java
@@ -0,0 +1,24 @@
+package ru.streamer.file_system.impl;
+
+import ru.streamer.file_system.PlayListProvider;
+
+import java.util.Map;
+
+public class FileSystemPlayListProvider implements PlayListProvider {
+
+
+
+
+
+
+    @Override
+    public Map<String, String> providePlayList() {
+        return null;
+    }
+
+
+
+
+
+
+}
