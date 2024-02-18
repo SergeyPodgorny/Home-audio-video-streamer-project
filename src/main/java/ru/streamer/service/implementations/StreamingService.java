2bacf27a351cb5f4b09b6a68cb599bcf38a09603
SergeyPodgorny <serkonpod@gmail.com>
2024-02-18 18:26:59 +0300

    added additional loging for debugging in windows

--- src/main/java/ru/streamer/service/implementations/StreamingService.java
+++ src/main/java/ru/streamer/service/implementations/StreamingService.java
@@ -16,7 +16,9 @@ public class StreamingService implements VideoProvider {
 
     private final PlayListInitialization playList;
     private final ResourceLoader resourceLoader;
-    private final static String routeFormat ="file:/%s";
+    private final String routeFormat ="file:/%s";
+    private final String titleFormat = "Played file title /%s";
+    private final String routeLogFormat = "Played file route /%s";
 
 
     @Autowired
@@ -27,7 +29,9 @@ public class StreamingService implements VideoProvider {
 
 
     public Mono<Resource> getVideo(String title){
+        log.info(String.format(titleFormat, title));
         var currentRoute = String.format(routeFormat,playList.getPlayList().get(title));
+        log.info(String.format(routeLogFormat,currentRoute));
         return Mono.fromSupplier(()-> resourceLoader.getResource(currentRoute));
     }
 
