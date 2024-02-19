adc6874d8c3e2520e9a85d3366c02bcfec5b76cf
SergeyPodgorny <serkonpod@gmail.com>
2024-02-19 17:10:33 +0300

    FileExtensionPredicates refactored

--- src/main/java/ru/streamer/playlist/predicates/FileExtensionPredicates.java
+++ src/main/java/ru/streamer/playlist/predicates/FileExtensionPredicates.java
@@ -1,42 +1,27 @@
 package ru.streamer.playlist.predicates;
 
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
 
-
+@Component
 public class FileExtensionPredicates {
 
     public static boolean mp4Test(Path path) {
         var extension = FilenameUtils.getExtension(String.valueOf(path.getFileName()));
-        if(extension.equalsIgnoreCase("mp4")){
-            return true;
-        } else {
-            return false;
-        }
-
+        return extension.equalsIgnoreCase("mp4");
     }
 
     public static boolean mkvTest(Path path) {
         var extension = FilenameUtils.getExtension(String.valueOf(path.getFileName()));
-        if(extension.equalsIgnoreCase("mkv")){
-            return true;
-        } else {
-            return false;
-        }
-
+        return extension.equalsIgnoreCase("mkv");
     }
 
 
     public static boolean aviTest(Path path) {
         var extension = FilenameUtils.getExtension(String.valueOf(path.getFileName()));
-        if(extension.equalsIgnoreCase("avi")){
-            return true;
-        } else {
-            return false;
-        }
-
+        return extension.equalsIgnoreCase("avi");
     }
-
 
 }
